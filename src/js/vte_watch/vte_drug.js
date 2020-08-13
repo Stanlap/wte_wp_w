aeebd436d0e2096648ac3307e990e821eee5c86e
Stanlap <stanlap2@gmail.com>
2020-08-13 10:45:07 +0300

    made drug without alert and modals

--- src/js/vte_watch/vte_drug.js
+++ src/js/vte_watch/vte_drug.js
@@ -1,18 +1,371 @@
 $(document).ready(function () {
+    oDrugsList = {
+        'Enoxaparin sodium': {
+            pair: {
+                'Enoxaparin sodium': 'Эноксапарин натрия'
+            },
+            'singleProphDose': 40,
+            officUnits: 'мл',
+            delivery: 'п/к',
+            container: 'шприц',
+            timesADay: 1,
+            potentTimeADay: [1, 2],
+            drugs: {
+                Anfibra: {
+                    nameCyr: 'Анфибра',
+                    nameLat: 'Anfibra',
+                    officDose: [0.2, 0.4, 0.6, 0.8, 1, ],
+                },
+                Hemapaxan: {
+                    nameCyr: 'Гемапаксан',
+                    nameLat: 'Hemapaxan',
+                    officDose: [0.4],
+                },
+                Clexane: {
+                    nameCyr: 'Клексан',
+                    nameLat: 'Clexane',
+                    officDose: [0.2, 0.4, 0.6, 0.8, 1, ],
+                },
+                'Flenox Neo': {
+                    nameCyr: 'Фленокс НЕО',
+                    nameLat: 'Flenox Neo',
+                    officDose: [0.2, 0.4, 0.6, 0.8, 1, ],
+                },
+                Enixum: {
+                    nameCyr: 'Эниксум',
+                    nameLat: 'Enixum',
+                    officDose: [0.2, 0.4, 0.6, 0.8, 1, ],
+                },
+                'Enoxaparin sodium': {
+                    nameCyr: 'Эноксапарин натрия',
+                    nameLat: 'Enoxaparin sodium',
+                    officDose: [0.2, 0.3, 0.4, 0.6, 0.8, 1, ],
+                },
+                'Enoxaparin Binergia': {
+                    nameCyr: 'Эноксапарин-Бинергия',
+                    nameLat: 'Enoxaparin Binergia',
+                    officDose: [0.2, 0.4, 0.6, 0.8, 1, ],
+                }
+            }
+        },
+        'Nadroparin calcium': {
+            pair: {
+                'Nadroparin calcium': 'Надропарин кальция'
+            },
+            'singleProphDose': 0.4,
+            officUnits: 'мл',
+            delivery: 'п/к',
+            container: 'шприц',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {
+                Fraxiparine: {
+                    nameCyr: 'Фраксипарин',
+                    nameLat: 'Fraxiparine',
+                    officDose: [0.3, 0.4, 0.6, 0.8, 1, ],
+                },
+                'Fraxiparine Forte': {
+                    nameCyr: 'Фраксипарин Форте',
+                    nameLat: 'Fraxiparine Forte',
+                    officDose: [0.6, 0.8, 1, ],
+                }
+            }
+        },
+        'Bemiparinum natrium': {
+            pair: {
+                'Bemiparinum natrium': 'Бемипарин натрия'
+            },
+            'singleProphDose': 3500,
+            officUnits: 'МЕ',
+            delivery: 'п/к',
+            container: 'шприц',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {                    
+            }
+        },
+        'Heparin sodium': {
+            pair: {
+                'Heparin sodium': 'Гепарин натрия'
+            },
+            'singleProphDose': 5000,
+            officUnits: 'МЕ',
+            delivery: 'п/к',
+            container: 'амп.',
+            timesADay: 3,
+            potentTimeADay: [2, 3],
+            drugs: {
+                Heparinum: {
+                    nameCyr: 'Гепарин',
+                    nameLat: 'Heparinum',
+                    officDose: [5000],
+                },
+                'Heparinum natrium': {
+                    nameCyr: 'Гепарин натрия',
+                    nameLat: 'Heparinum natrium',
+                    officDose: [5000],
+                }
+            }
+        },
+        'Fondaparinux sodium': {
+            pair: {
+                'Fondaparinux sodium': 'Фондапаринукс натрия'
+            },
+            'singleProphDose': 0.5,
+            officUnits: 'мл',
+            delivery: 'п/к',
+            container: 'шприц',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {
+                Arixrta: {
+                    nameCyr: 'Арикстра',
+                    nameLat: 'Arixrta',
+                    officDose: [0.5, 1, ],
+                }
+            }
+        },
+        'Acetylsalicylic acid': {
+            pair: {
+                'Acetylsalicylic acid': 'Ацетилсалициловая кислота'
+            },
+            'singleProphDose': 100,
+            officUnits: 'мг',
+            delivery: 'внутрь',
+            container: 'таб.',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {
+                'Acetylsalicylic acid': {
+                    nameCyr: 'Ацетилсалициловая кислота',
+                    nameLat: 'Acetylsalicylic acid',
+                    officDose: [500],
+                },
+
+                'Aspirin Cardio': {
+                    nameCyr: 'Аспирин кардио',
+                    nameLat: 'Aspirin Cardio',
+                    officDose: [100, 300],
+                },
+
+                'ASK-cardio': {
+                    nameCyr: 'АСК-кардио',
+                    nameLat: 'ASK-cardio',
+                    officDose: [100],
+                },
+
+                Acecardol: {
+                    nameCyr: 'Ацекардол',
+                    nameLat: 'Acecardol',
+                    officDose: [50, 100, 300],
+                },
+
+                Trombopol: {
+                    nameCyr: 'Тромбопол',
+                    nameLat: 'Trombopol',
+                    officDose: [75, 175],
+                },
+
+                Sanovasc: {
+                    nameCyr: 'Сановаск',
+                    nameLat: 'Sanovasc',
+                    officDose: [50, 75, 100],
+                },
+
+                'Thrombo ASS': {
+                    nameCyr: 'Тромбо АСС',
+                    nameLat: 'Thrombo ASS',
+                    officDose: [50, 100],
+                },
+
+                CardiASK: {
+                    nameCyr: 'КардиАСК',
+                    nameLat: 'CardiASK',
+                    officDose: [50, 100, 300],
+                },
+
+                Aspicor: {
+                    nameCyr: 'Аспикор',
+                    nameLat: 'Aspicor',
+                    officDose: [100],
+                },
+
+                'Upsarin UPSA': {
+                    nameCyr: 'Упсарин УПСА',
+                    nameLat: 'Upsarin UPSA',
+                    officDose: [500],
+                }
+            }
+        },
+        'Dabigatran etexilate': {
+            pair: {
+                'Dabigatran etexilate': 'Дабигатрана этексилат'
+            },
+            'singleProphDose': 150,
+            officUnits: 'мг',
+            delivery: 'внутрь',
+            container: 'капс.',
+            timesADay: 2,
+            potentTimeADay: [1, 2],
+            drugs: {
+                Pradaxa: {
+                    nameCyr: 'Прадакса',
+                    nameLat: 'Pradaxa',
+                    officDose: [75, 150],
+                }
+            }
+        },
+        'Rivaroxaban': {
+            pair: {
+                'Rivaroxaban': 'Ривароксабан'
+            },
+            'singleProphDose': 20,
+            officUnits: 'мг',
+            delivery: 'внутрь',
+            container: 'таб.',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {
+                Xarelto: {
+                    nameCyr: 'Ксарелто',
+                    nameLat: 'Xarelto',
+                    officDose: [2.5, 10, 15, 20],
+                }
+            }
+        },
+        'Apixaban': {
+            pair: {
+                'Apixaban': 'Апиксабан'
+            },
+            'singleProphDose': 2.5,
+            officUnits: 'мг',
+            delivery: 'внутрь',
+            container: 'таб.',
+            timesADay: 1,
+            potentTimeADay: [1, 2],
+            drugs: {
+                Eliquis: {
+                    nameCyr: 'Эликвис',
+                    nameLat: 'Eliquis',
+                    officDose: [2.5, 5],
+                }
+            }
+        },
+        'Edoxaban': {
+            pair: {
+                'Edoxaban': 'Эдоксабан'
+            },
+            'singleProphDose': 60,
+            officUnits: 'мг',
+            delivery: 'внутрь',
+            container: 'таб.',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {
+                Lixiana: {
+                    nameCyr: 'Ликсиана',
+                    nameLat: 'Lixiana',
+                    officDose: [15, 30, 60],
+                }
+            }
+        },
+        'Warfarin': {
+            pair: {
+                'Warfarin': 'Варфарин'
+            },
+            'singleProphDose': 5.0,
+            officUnits: 'мг',
+            delivery: 'внутрь',
+            container: 'таб.',
+            timesADay: 1,
+            potentTimeADay: [1],
+            drugs: {
+                Warfarex: {
+                    nameCyr: 'Варфарекс',
+                    nameLat: 'Warfarex',
+                    officDose: [1, 3, 5],
+                },
+                Warfarin: {
+                    nameCyr: 'Варфарин',
+                    nameLat: 'Warfarin',
+                    officDose: [2.5],
+                },
+                Warfarin: {
+                    nameCyr: 'Варфарин Канон',
+                    nameLat: 'Warfarin',
+                    officDose: [2.5],
+                },
+                'Warfarin Nycomed': {
+                    nameCyr: 'Варфарин Никомед',
+                    nameLat: 'Warfarin Nycomed',
+                    officDose: [2.5],
+                },
+                'Warfarin-OBL': {
+                    nameCyr: 'Варфарин-OBL',
+                    nameLat: 'Warfarin-OBL',
+                    officDose: [2.5],
+                }
+            },
+        }
+    },
+    
+    oChoosDrug = {
+        titleGroupCyr: '',
+        titleGroupLat: '',
+        titleDrugLat: '',
+        titleDrugCyr: '',
+        officDose: 0,
+        singleDoseOfAspirin: 0,
+        sheduleAspirinTakingDaily: '',
+        singleProphDose: '',
+        tempCont: '',
+        signature: '',
+        realSingleDose: 0,
+        numberOfOfficDose: 1,
+        frequencyOfDrugTaking: 'сут.',
+        oOfficDose: {},
+        startDateOfVTEProphyl: '', 
+        endDateOfVTEProphyl: '',
+        minTreatPeriod: 0,
+        aLine: []
+    };
+
+    function correctDate(vD) {
+        return `${vD.getFullYear()}-${('0' + (vD.getMonth() + 1)).slice(-2)}-${('0' + vD.getDate()).slice(-2)}`;
+    }
+    function addDays(date, days) {
+        let result = new Date(date);
+        result.setDate(result.getDate() + days);
+        return result;
+    }
+    function formatDate() {
+        let d = new Date(),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear(),
+            vDateNow = '';
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+        vDateNow = [year, month, day].join('-');
+        return vDateNow;
+    }
+    function diffDates(dateOne, dateTwo) {
+        return (dateOne - dateTwo) / (60 * 60 * 24 * 1000);
+    }
+
     const initModal = (quest, ind) => {
         $(`<div class="modal" tabindex="-1" role="dialog" id="#divModal_${ind}" data-backdrop="static"><div class="modal-dialog" role="document" ><div class="modal-content"><div class="modal-body"><p>${quest}</p></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal"  id="btnMYes_${ind}">Да</button><button type="button" class="btn btn-secondary" data-dismiss="modal" id="btnMNo_${ind}">Нет</button></div></div></div></div>`).appendTo('body').show();
     };
+
+
+
     let vTakesVTEProph = false;
-    let modal = document.getElementById('modYN_1');
 
-    $('#btn1').on('click', function(){
-    modal.style.display = "block";
-    });
-
-    initModal('Пациент уже получает антикоагулянтную терапию?', 1);
-    $('#btnMYes_1').on('click', () => {
-        vTakesVTEProph = true;
-    });
+    // initModal('Пациент уже получает антикоагулянтную терапию?', 1);
+    // $('#btnMYes_1').on('click', () => {
+    //     vTakesVTEProph = true;
+    //     $(this).hide();
+    // });
 
     let aPat = [], aStepBack= [];
     aPat = JSON.parse(localStorage.getItem('Patient'));
@@ -425,7 +778,7 @@ $(document).ready(function () {
     function defineRealSingleDoseOfMedicine() {
         console.log('defineRealSingleDoseOfMedicine');
         let tRSD = correctEnteredValue($('#inpText_0').val());
-        !isFinite(tRSD) || tRSD === 0 ? (initAlert('Вы ввели некорректное значение').prependTo('#dialog_0'), $('#inpText_0').val('').focus()) : (oChoosDrug.realSingleDose = correctEnteredValue($('#inpText_0').val()), $('#inpText_0').hide(), $('#inpText_0').off('input'), clearValues(), executeFuncsLine(), $('#inpText_0').remove(), $().alert('dispose'));
+        !isFinite(tRSD) || tRSD === 0 ? $('#inpText_0').val('').focus() : (oChoosDrug.realSingleDose = correctEnteredValue($('#inpText_0').val()), $('#inpText_0').hide(), $('#inpText_0').off('input'), clearValues(), executeFuncsLine(), $('#inpText_0').remove());
     }
 
     function correctEnteredValue(el) {
@@ -628,7 +981,6 @@ $(document).ready(function () {
             sheduleSplitPeriod();
         };
         interactOfXaInhibAndVKA();
-        $().alert('dispose');
         clearValues();
         executeFuncsLine();
     }
@@ -766,10 +1118,7 @@ $(document).ready(function () {
             oPat.pkINRDates[1] = oPat.pkINRDates[0] + 1;
             console.log(oPat.pkINRDates);
             if (diffDates(new Date(aStartDatesNative[findIndXaInhibitors(aTitles, vInd = -1)]), new Date(aStartDatesNative[aTitles.indexOf('Варфарин')])) < 0) {
-
-                initAlert('При переходе с НОАК на АВК стоит иметь в виду, что НОАК влияют на МНО. Для более адекватного определения степени антикоагуляции при одновременном приеме НОАК и АВК МНО необходимо определять непосредственно перед приемом очередной дозы НОАК и через 24 часа после приема последней дозы НОАК.').prependTo('#divMainDialog');
             } else {
-                initAlert('НОАК  после АВК могут быть назначены в этот же или на следующий день при значении МНО 2,0-2,5. Ривароксабан может быть назначен при МНО ≤3,0; эдоксабан – при МНО≤2,5; апиксабан и дабигатран – при МНО ≤2,0. Если значения превышают указанные, повторяют исследование МНО, при достижении указанных показателей назначают препарат.').prependTo('#divMainDialog');
                 $(aChMeds).each((ind, el) => {
                     vXaInhibitors(el.titleGroupCyr) ? el.aLine = [] : '';
                 });
@@ -850,7 +1199,6 @@ let vBridge = false;
             vBridge = true;
         });
         !vBridge ? (clearValues(), executeFuncsLine()) : (
-            oPat.pkIsSpinalAnesth ? initAlert('При спинальной анестезии предпочтительно использовать НМГ.').prependTo('#divDialogMain') : '',
             $('#invitToAct_1').html('Выберите группу препарата для мост-терапии:'),
             $('#dialog_1').show(),
             oDrugsList['Bemiparinum natrium'].drugs.hasOwnProperty('Zibor 2500') ? (delete oDrugsList['Bemiparinum natrium'].drugs['Zibor 2500'],
@@ -867,7 +1215,6 @@ let vBridge = false;
 
     function defineBridgeTherUsage() {
         console.log('defineBridgeTherUsage');
-        $().alert('dispose');
         oPat.pkBridgeTherMedGroup = $('#select_1 :selected').val();
         console.log(oPat.pkBridgeTherMedGroup);
         sheduleSplitPeriod();
@@ -959,14 +1306,14 @@ let vBridge = false;
 
 
     function askOfPrevLabExams() {
-
-        initModal('Назначать лабораторные исследования в субботу и воскресенье?', 6);
-        $('#btnMYes_6').on('click', () => {
-            oPat.pkIncludeWeekends = true;
-        });
-        $('#btnMNo_6').on('click', () => {
-            oPat.pkIncludeWeekends = false;
-        });
+        // initModal('Назначать лабораторные исследования в субботу и воскресенье?', 6);
+        // $('#btnMYes_6').on('click', () => {
+        //     oPat.pkIncludeWeekends = true;
+        // });
+        // $('#btnMNo_6').on('click', () => {
+        //     oPat.pkIncludeWeekends = false;
+        // });
+        oPat.pkIncludeWeekends = true;
         $('#invitToAct_1').html('До начала профилактивки ВТЭО необходимо наличие перечисленных ниже исследований. Если обследование неполное, установите дату и отметьте, какие исследования требуется выполнить:');
         let vTom = addDays(new Date(), 1);
         new Date().getHours() < 10 ? $('#inpDate_4').val(formatDate()) : $('#inpDate_4').val(`${vTom.getFullYear()}-${('0' + (vTom.getMonth() + 1)).slice(-2)}-${('0' + vTom.getDate()).slice(-2)}`);
